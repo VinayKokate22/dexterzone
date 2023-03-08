57bc3bda71626c6aa4e23f2404fab2286d06bed6
Vinay Kokate <vinaynkokate@gmail.com>
2023-03-08 16:38:16 +0530

    made it responsive

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
+const aboutSkills = document.querySelector(".aboutskills");
+const aboutExp = document.querySelector(".aboutexp");
+const aboutEdu = document.querySelector(".aboutedu");
+const tabLinks = document.querySelector(".tab-links-skills");
+const tabLinksExp = document.querySelector(".tab-links-exp");
+const tabLinksEdu = document.querySelector(".tab-links-edu");
 
+const colorPalet = document.querySelector(".colorpalet");
+const colorOption = document.querySelector(".color-option");
 
-
-const aboutSkills= document.querySelector(".aboutskills")
-const aboutExp= document.querySelector(".aboutexp")
-const aboutEdu= document.querySelector(".aboutedu")
-const tabLinks= document.querySelector(".tab-links-skills")
-const tabLinksExp= document.querySelector(".tab-links-exp")
-const tabLinksEdu= document.querySelector(".tab-links-edu")
-
-const colorPalet=document.querySelector(".colorpalet")
-const colorOption=document.querySelector(".color-option")
-
-
-const theme=document.querySelector(".theme")
-const faMoon=document.querySelector(".fa-moon")
+const theme = document.querySelector(".theme");
+const faMoon = document.querySelector(".fa-moon");
 
 // faMoon.addEventListener("click",function () {
 //     theme.classList.add("light")
 //     colorPalet.classList.add("hidden")
 //     document.documentElement.style.setProperty('--colortheme',"#5793fb")
 //     document.documentElement.style.setProperty('--divbackground',"#afdaff")
-   
-    
-    
+
 // })
 
 // faMoon.addEventListener("dblclick",function () {
@@ -30,36 +24,31 @@ const faMoon=document.querySelector(".fa-moon")
 //     colorPalet.classList.remove("hidden")
 //     document.documentElement.style.setProperty('--colortheme',"#5067cd")
 //     document.documentElement.style.setProperty('--divbackground',"#212121")
-    
+
 // })
 
+faMoon.addEventListener("click", function () {
+  theme.classList.toggle("light");
+  colorPalet.classList.toggle("hidden");
+  if (theme.classList.contains("light")) {
+    document.documentElement.style.setProperty("--colortheme", "#5793fb");
+    document.documentElement.style.setProperty("--divbackground", "#afdaff");
+  } else {
+    document.documentElement.style.setProperty("--colortheme", "#5067cd");
+    document.documentElement.style.setProperty("--divbackground", "#212121");
+  }
+}) /
+  //
+  //
+  //
+  //
+  colorPalet.addEventListener("click", function () {
+    colorOption.classList.toggle("hidden");
+  });
 
-faMoon.addEventListener("click", function() {
-    theme.classList.toggle("light")
-    colorPalet.classList.toggle("hidden")
-    if (theme.classList.contains("light")) {
-      document.documentElement.style.setProperty("--colortheme", "#5793fb")
-      document.documentElement.style.setProperty("--divbackground", "#afdaff")
-    } else {
-      document.documentElement.style.setProperty("--colortheme", "#5067cd")
-      document.documentElement.style.setProperty("--divbackground", "#212121")
-    }
-  })
-
-// 
-// 
-// 
-// 
-/
-
-
-colorPalet.addEventListener("click",function () {
-    colorOption.classList.toggle("hidden")
-})
-
-// 
-// 
-// 
+//
+//
+//
 
 // old code
 
@@ -79,8 +68,8 @@ colorPalet.addEventListener("click",function () {
 //                     document.documentElement.style.setProperty('--colortheme',color)
 //     colorOption.classList.toggle("hidden")
 
-//                 })  
-                
+//                 })
+
 //                 const Colors3=document.querySelector(".green")
 // Colors3.addEventListener("click",function () {
 //             console.log("hello")
@@ -90,70 +79,47 @@ colorPalet.addEventListener("click",function () {
 
 //         })
 
-const colorOptions = document.querySelectorAll(".colortheme")
-colorOptions.forEach(colorOption => {
-  colorOption.addEventListener("click", function() {
-    let color = this.getAttribute("data-color")
-    document.documentElement.style.setProperty("--colortheme", color)
+const colorOptions = document.querySelectorAll(".colortheme");
+colorOptions.forEach((colorOption) => {
+  colorOption.addEventListener("click", function () {
+    let color = this.getAttribute("data-color");
+    document.documentElement.style.setProperty("--colortheme", color);
     // colorOption.classList.toggle("hidden")
-  })
-})
+  });
+});
 
+tabLinks.addEventListener("click", function () {
+  aboutSkills.classList.add("active-tab");
+  aboutExp.classList.remove("active-tab");
+  aboutEdu.classList.remove("active-tab");
+});
+tabLinksExp.addEventListener("click", function () {
+  aboutExp.classList.add("active-tab");
+  aboutEdu.classList.remove("active-tab");
+  aboutSkills.classList.remove("active-tab");
+});
+tabLinksEdu.addEventListener("click", function () {
+  aboutEdu.classList.add("active-tab");
+  aboutExp.classList.remove("active-tab");
+  aboutSkills.classList.remove("active-tab");
+});
 
-// tabLinks.addEventListener("click",function () {
-//     aboutSkills.classList.add("active-tab")
-//     aboutExp.classList.remove("active-tab")
-//     aboutEdu.classList.remove("active-tab")
-
-// })
-// tabLinksExp.addEventListener("click",function () {
-//     aboutExp.classList.add("active-tab")
-//     aboutEdu.classList.remove("active-tab")
-//     aboutSkills.classList.remove("active-tab")
-
-
-// })
-// tabLinksEdu.addEventListener("click",function () {
-//    aboutEdu.classList.add("active-tab")
-//    aboutExp.classList.remove("active-tab")
-//    aboutSkills.classList.remove("active-tab")
-
-
-// })
-
-tabLinks.addEventListener("click", function(event) {
-    if (event.target.classList.contains("tab-links-skills")) {
-      aboutSkills.classList.add("active-tab")
-      aboutExp.classList.remove("active-tab")
-      aboutEdu.classList.remove("active-tab")
-    } else if (event.target.classList.contains("tab-links-exp")) {
-      aboutExp.classList.add("active-tab")
-      aboutEdu.classList.remove("active-tab")
-      aboutSkills.classList.remove("active-tab")
-    } else if (event.target.classList.contains("tab-links-edu")) {
-      aboutEdu.classList.add("active-tab")
-      aboutExp.classList.remove("active-tab")
-      aboutSkills.classList.remove("active-tab")
-    }
-  })
-  
-
-
-function sendemail() {
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "prathamvahuja@gamil.com",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}
-
+// tabLinks.addEventListener("click", function(event) {
+//     if (event.target.classList.contains("tab-links-skills")) {
+//       aboutSkills.classList.add("active-tab")
+//       aboutExp.classList.remove("active-tab")
+//       aboutEdu.classList.remove("active-tab")
+//     } else if (event.target.classList.contains("tab-links-exp")) {
+//       aboutExp.classList.add("active-tab")
+//       aboutEdu.classList.remove("active-tab")
+//       aboutSkills.classList.remove("active-tab")
+//     } else if (event.target.classList.contains("tab-links-edu")) {
+//       aboutEdu.classList.add("active-tab")
+//       aboutExp.classList.remove("active-tab")
+//       aboutSkills.classList.remove("active-tab")
+//     }
+//   })
 
 const Scrollbar = window.scrollbars;
 
-Scrollbar.init(document.querySelector("#smoothscroll"))
+Scrollbar.init(document.querySelector("#smoothscroll"));
